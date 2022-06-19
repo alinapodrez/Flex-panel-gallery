bcb4d71db0c60cd818a36ccfe6f2ccb1f6193b2c
alinapodrez <95874838+alinapodrez@users.noreply.github.com>
2022-06-19 14:56:42 +0300

    Add files via upload
    updated js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const images = document.querySelectorAll('.img');
+/*
+//1
 
-    function toggleOpen() {
-      this.classList.toggle('open');
-    }
+function toggleOpen() {
+this.classList.toggle('open');
+}
 
-    function toggleActive(e) {
-      if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-      }
-    }
+function toggleActive(e) {
+if (e.propertyName.includes('flex')) {
+ this.classList.toggle('open-active');
+}
+}
 
-    images.forEach(images => images.addEventListener('click', toggleOpen));
-    images.forEach(images => images.addEventListener('transitionend', toggleActive));
+images.forEach(images => images.addEventListener('click', toggleOpen));
+images.forEach(images => images.addEventListener('transitionend', toggleActive)); 
 
-   /* images.onclick = () =>{
-      images.classList.toggle('open');
-      images.classList.toggle('open-active');
-    } */
+//2
+
+images.forEach(image => image.onclick = () => {
+  image.classList.toggle('open');
+  image.classList.toggle('open-active');
+});
+*/
+
+//3
+
+images.forEach(image => image.addEventListener("click", () => {
+  image.classList.toggle('open');
+  image.classList.toggle('open-active');
+}));
